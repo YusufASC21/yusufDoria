48556fbc71962f56691721536bdaae0501471022
Yusuf Doria <22doriay@bsge.org>
2021-08-09 11:08:54 -0400

    nav

--- script.js
+++ script.js
@@ -7,15 +7,3 @@ function revertColor (x) {
     x.style.backgroundColor  = "#2F3061";
     x.style.color  = "#F7FFF7";
 }
-
-function cardBig (x) {
-    console.log("hi");
-    x.style.height = "150%";
-    x.style.width = "150%";
-}
-
-function revertCard (x) {
-    console.log("hi");
-    x.style.height = "100%";
-    x.style.width = "100%";
-}
