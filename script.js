71cd491e41263de402010bff0243d8391ccd5102
Yusuf Doria <22doriay@bsge.org>
2021-08-08 20:24:16 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("HELLO")
+function colorChange (x) {
+    x.style.backgroundColor  = "#ECF0F1";
+    x.style.color  = "#2F3061";
+}
+
+function revertColor (x) {
+    x.style.backgroundColor  = "#2F3061";
+    x.style.color  = "#F7FFF7";
+}
+
+function cardBig (x) {
+    console.log("hi");
+    x.style.height = "150%";
+    x.style.width = "150%";
+}
+
+function revertCard (x) {
+    console.log("hi");
+    x.style.height = "100%";
+    x.style.width = "100%";
+}
